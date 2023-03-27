0045a4fbc46532fc731d8d79a65ef307208b8d3a
callum-jpg <callumtc.jpg@gmail.com>
2023-03-27 10:39:12 -0400

    update library module to reflect library function changes

--- cellprofiler/library/modules/_measureimageoverlap.py
+++ cellprofiler/library/modules/_measureimageoverlap.py
@@ -1,21 +1,32 @@
-from cellprofiler.library.functions.measurement import measure_image_overlap
+from cellprofiler.library.functions.measurement import (
+    measure_image_overlap_statistics,
+    compute_earth_movers_distance,
+)
 
 
 def measureimageoverlap(
     ground_truth_image,
     test_image,
     mask=None,
+    calculate_emd=False,
     max_distance=250,
     penalize_missing=False,
     decimation_method="k_means",
     max_points=250,
 ):
-    return measure_image_overlap(
-        ground_truth_image,
-        test_image,
-        mask=mask,
-        max_distance=max_distance,
-        penalize_missing=penalize_missing,
-        decimation_method=decimation_method,
-        max_points=max_points,
+
+    data = measure_image_overlap_statistics(
+        ground_truth_image=ground_truth_image, test_image=test_image, mask=mask
     )
+
+    if calculate_emd:
+        emd = compute_earth_movers_distance(
+            ground_truth_image=ground_truth_image,
+            test_image=test_image,
+            max_distance=max_distance,
+            penalize_missing=penalize_missing,
+            decimation_method=decimation_method,
+            max_points=max_points,
+        )
+        data.update({"EarthMoversDistance": emd})
+    return data
